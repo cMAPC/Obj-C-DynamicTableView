6f87bb2c3f40ee629fa586168e48776525bda8f6
Marcel <marcel@Marcels-MacBook-Pro.local>
2017-02-02 19:21:39 +0300

    Initial Commit

---
+++ TableViewDynamicSize/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TableViewDynamicSize
+//
+//  Created by Marcel  on 2/2/17.
+//  Copyright © 2017 Marcel . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
